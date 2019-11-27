09b0a017483908f2a79b7d23ba71b78f63a0d49d
KnightHart <chaosdaimao@gmail.com>
2019-11-27 16:38:23 +0200

    Changed Stages Layout

--- Assets/Scripts/GoalController.cs
+++ Assets/Scripts/GoalController.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoalController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider col)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = currentScene + 1;
+        if(col.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextScene);
+        }
     }
 }
